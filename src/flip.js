793808269edd23d4ad0e2169aa59fb296b583d72
Kristian Nygaard Jensen <freeduck@member.fsf.org>
2021-08-13 23:05:38 +0200

    Pages are turning

---
+++ src/flip.js
@@ -0,0 +1,8 @@
+import {PageFlip} from 'page-flip';
+const pageFlip = new PageFlip(document.getElementById('book'),
+    {
+        width: 400, // required parameter - base page width
+        height: 600,  // required parameter - base page height
+        showCover: true
+    }
+);
